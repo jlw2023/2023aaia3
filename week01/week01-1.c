bbc2caabb725400f072e4fd3b503aaf69a6af504
jlw2023 <144758487+jlw2023@users.noreply.github.com>
2024-09-12 16:29:58 +0800

    week01

---
+++ week01/week01-1.c
@@ -0,0 +1,12 @@
+//week01-1.c
+# include <stdio.h>
+int main()
+
+{
+    int n;
+    scanf("%d", &n);
+    int d50=n/50;
+    int d5=n%50/5;
+    int d1=n%5;
+    printf("%d %d %d\n", n, d50, d5, d1);
+}
